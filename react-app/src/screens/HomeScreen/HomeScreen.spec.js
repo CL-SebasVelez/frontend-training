4a0851349c48289a66314d44ec768fc2229d1eb8
SebasVelez34 <sebastianvelezvelasquez@gmail.com>
2020-10-05 14:07:58 -0500

    Testing components and HomeScreen

--- react-app/src/screens/HomeScreen/HomeScreen.spec.js
+++ react-app/src/screens/HomeScreen/HomeScreen.spec.js
@@ -6,6 +6,9 @@ import sinon from 'sinon';
 import ContactForm from '../../components/ContactForm';
 import Navbar from '../../components/Navbar';
 import HomeScreen from './HomeScreen.js';
+import Alert from 'emerald-ui/lib/Alert/Alert';
+import Spinner from 'emerald-ui/lib/Spinner/Spinner';
+import NewsCard from '../../components/NewsCard/index.js';
 
 describe('HomeScreen', () => {
   var wrapper;
@@ -13,12 +16,23 @@ describe('HomeScreen', () => {
     wrapper = mount(<HomeScreen />);
   });
 
-  it('Should exits Navbar component', () => {
+  it('Should exist Navbar component', () => {
     let navbarComponent = wrapper.find(Navbar);
     expect(navbarComponent).to.have.lengthOf(1);
   });
-  it('Should exits ContactForm component', () => {
+
+  it('Should exist ContactForm component', () => {
     let contactFormComponent = wrapper.find(ContactForm);
     expect(contactFormComponent).to.have.lengthOf(1);
+  });
+
+  it('Should exist Alert component', () => {
+    let alertComponent = wrapper.find(Alert);
+    expect(alertComponent).to.have.lengthOf(1);
+  });
+
+  it('Should exist Spinner component', () => {
+    let spinnerComponent = wrapper.find(Spinner);
+    expect(spinnerComponent).to.have.lengthOf(1);
   });
 });
